3ff16106fe10a4124ccaf6e0dcdaf897a9fa4a55
First Last <abiralbh.101@gmail.com>
2024-03-10 19:00:37 +0545

    final commit

--- array.py
+++ array.py
@@ -7,7 +7,7 @@ while(True):
     choice = int(input("\n1.push\n2.pop\n3.Exit\n"))
     if(choice == 1):
         a = int(input("\nEnter number:"))
-        if(len(stack) == size):
+        if(tos == size):
             print("Stack Overflow")
         else:
             stack.append(a)
@@ -16,9 +16,6 @@ while(True):
             print(f"TOS:{stack[tos-1]}")
     if(choice == 2):
         try:
-            if(len(stack) == 0):
-                print("Stack Underflow")
-            else:
                 tos-=1
                 print(f"\nPopped {stack.pop()}")
                 print(f"\nTOS:{stack[tos-1]}")
